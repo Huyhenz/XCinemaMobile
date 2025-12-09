468ec8d3a8154fe062bf51a1d4a3e4905ad113e7
Huyhenz <tudomb2004@gmail.com>
2025-12-09 20:36:27 +0700

    Updated dictionary

---
+++ ltdd/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
